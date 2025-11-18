18f6508257af3efdea9a43a1bbca51bbe9fb44fe
Mukesh-cyberr <mukeshrajdhayanidhi2006@gmail.com>
2025-11-18 05:02:38 +0000

    Initial commit

--- execcheck2.c
+++ execcheck2.c
@@ -1,1 +1,30 @@
-//C Program to execute Linux system commands using Linux API system calls exec() family
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main() 
+{
+    pid_t pid;
+
+    // create a new process
+    pid = fork();
+
+    if (pid < 0) {
+        perror("fork failed");
+        return 1;
+    }
+
+    if (pid == 0) {
+        // Child process
+        printf("Child Process:\n");
+        printf("  PID: %d\n", getpid());
+        printf("  Parent PID: %d\n", getppid());
+    } else {
+        // Parent process
+        printf("Parent Process:\n");
+        printf("  PID: %d\n", getpid());
+        printf("  Child PID: %d\n", pid);
+    }
+
+    return 0;
+}
